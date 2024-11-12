cf7d0abe16d3dfa791f761d3ba1490a80573c61f
Aria Taylor <ari@willing.com>
2024-11-12 13:14:21 -0800

    Add notarization records resource

--- proof_business_api/resource.py
+++ proof_business_api/resource.py
@@ -5,12 +5,14 @@ from .client import Client
 from .transactions import TransactionsClient
 from .documents import DocumentsClient
 from .webhooks import WebhooksClient
+from .records import NotarizationRecordsClient
 
 
 class ProofClient:
     transactions = TransactionsClient("")
     documents = DocumentsClient("")
     webhooks = WebhooksClient("")
+    records = NotarizationRecordsClient("")
 
     def __init__(self, *args, **kwargs) -> None:
         for name, member in inspect.getmembers(self):
